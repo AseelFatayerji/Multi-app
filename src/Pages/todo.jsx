0d9be3ef4697e950618b1c218fa98272e204c33e
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-03-28 12:35:08 +0200

    Created draggable object

--- src/Pages/todo.jsx
+++ src/Pages/todo.jsx
@@ -1,12 +1,20 @@
 import "../CSS/App.css";
 import Navbar from "./navbar";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faClipboard,faCloud,faCalculator } from "@fortawesome/free-solid-svg-icons"
+import Draggable, {DraggableCore} from "react-draggable";
 function Todo() {
   return (
     <div className="Todo">
       <Navbar />
       <div className="float-container space-even">
-        <div className="toolkit"></div>
+        <div className="toolkit">
+          <Draggable>
+          <div className="list">
+            <label>New card</label>
+            <input type="text" />
+          </div></Draggable>
+        </div>
         <div className="list"></div>
       </div>
     </div>
