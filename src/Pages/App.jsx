68c43af8e9e9d126f4c5e4f88d00586dcea2fed6
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2025-01-18 22:07:12 +0200

    fixed urls

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Multi-app">
      <Routes>
       <Route path='/' element={<Weather/>}/>
       <Route path='/todo' element={<Todo/>}/>
